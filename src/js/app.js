101e08dd415f926c0a1d8028f18f6c94dad1e34b
dwi joko purnomo <dwijokopurnomo10@gmail.com>
2020-02-10 18:35:14 +0700

    adding file

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+var message = document.getElementById("message");
+
+message.innerHTML = "Terimakasih sudah bealajar Gulp!";
+
+// slick
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
